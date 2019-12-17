11384aab3b1f6428fe81d06886389ccc41b74238
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2019-12-17 23:12:27 +0900

    Initial commit

---
+++ src/main/java/com/wemakeprice/junior/carpool/CarpoolApplication.java
@@ -0,0 +1,13 @@
+package com.wemakeprice.junior.carpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarpoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wemakeprice/junior/carpool/CarpoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wemakeprice.junior.carpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
